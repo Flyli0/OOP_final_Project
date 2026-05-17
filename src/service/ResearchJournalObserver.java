d2029d3e1c45cf2bc3252e259b8aad95fa7c9e3d
Yelnur <elnur.sul06@gmail.com>
2026-05-17 22:58:35 +0500

    Comm

--- src/service/ResearchJournalObserver.java
+++ src/service/ResearchJournalObserver.java
@@ -11,37 +11,21 @@ import model.User;
  */
 public class ResearchJournalObserver {
 
-    /**
-     * Default constructor
-     */
+    
     public ResearchJournalObserver() {
     }
 
-    /**
-     * 
-     */
     private List<ResearchProject> researchProjects;
 
-    /**
-     * 
-     */
     private List<User> subscribers;
 
-    /**
-     * @param User u 
-     * @return
-     */
+    
     public void addSubscriber(User u) {
-        // TODO implement here
         subscribers.add(u);
     }
-    /**
-     * @param User u 
-     * @return
-     */
+   
 
     public void removeSubscriber(User u) {
-        // TODO implement here
         if(subscribers.contains(u)){
             subscribers.remove(u);
         } else {
@@ -51,10 +35,7 @@ public class ResearchJournalObserver {
         
     }
 
-    /**
-     * @param String message 
-     * @return
-     */
+   
     public void notify( String message) {
         for(User subscriber : subscribers) {
             subscriber.update(message);
@@ -62,18 +43,12 @@ public class ResearchJournalObserver {
         
     }
 
-    /**
-     * @return
-     */
+    
     public List<User> getSubscribers() {
-        // TODO implement here
         return subscribers;
     }
 
-    /**
-     * @param ResearchProjet rp 
-     * @return
-     */
+    
     public void addResearch(ResearchProject rp) {
         researchProjects.add(rp);
         notify("New research project added: " + rp.getTopic());
